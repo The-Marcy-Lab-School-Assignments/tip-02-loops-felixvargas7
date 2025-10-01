03f1a2c1176da1196f5792c2b85a23ab1ea81a2a
Felix Vargas <felix70529@gmail.com>
2025-10-01 14:20:43 -0400

    Worked on index.js c hallenge problem, for loops

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // Write a function that prints the first n square numbers and also calculates the sum of these numbers.
 const printSquareNumbers = (n) => {
   //write code here
-
-}
+  let sqr = 0
+  for (let i = 1; i <= n; i++) {
+    let sqr = i * i
+    console.log(sqr);
+    sqr += i
+  }
+  return sqr
+};
 printSquareNumbers(5);
 //will print: 1, 4, 9, 16, 25
 // Final output is: 55
